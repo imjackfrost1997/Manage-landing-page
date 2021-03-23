f588004e960d30cd89a636e4bebe9e10a6ce211e
Robert Sampang <maravilloroan@gmail.com>
2021-03-23 23:22:58 +0800

    Manage landing page

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+});
